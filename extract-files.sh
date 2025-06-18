d8ec4f90e8cdb15b2a15efb38c647915e0b7db02
YagizErdemir06 <erdemiryagiz@proton.me>
2025-06-18 14:47:37 +0545

    rubyx: Patch `mtkfusionrild` to load v32 `libutils`
    RIL is dead on android 16 and this fixes the issue
    
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -140,6 +140,10 @@ function blob_fixup {
             [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
+        vendor/bin/hw/mtkfusionrild)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
+            ;;
         *)
             return 1
             ;;
