d3e67123b9c6ecd7f4a5e9d5deb7c27722a3c19b
YagizErdemir06 <erdemiryagiz@proton.me>
2025-10-26 17:04:24 +0100

    rubyx: Ship libbase_shim as product package
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- device.mk
+++ device.mk
@@ -341,6 +341,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Build dependencies
+PRODUCT_PACKAGES += \
+    libbase_shim
+
 # Reduce system server verbosity.
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 PRODUCT_OTHER_JAVA_DEBUG_INFO := false
